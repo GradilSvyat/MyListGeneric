3ace3af8366bd6d139bd0afa6f4357cb12ae1d70
GradilSvyat <43150486+GradilSvyat@users.noreply.github.com>
2020-04-29 12:34:49 +0300

    Add method "Add"

--- ConsoleApp1/MyList.cs
+++ ConsoleApp1/MyList.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyListGeneric
 {
-    class MyList
+    class MyList<T> : IEnumerable<T>
     {
+        MyClassGeneric<T> head;
+        MyClassGeneric<T> tail;
+        int count;
+        public void Add (T data)
+        {
+            MyClassGeneric<T> elementMyClass = new MyClassGeneric<T>(data);
+            if (head == null)
+                head = elementMyClass;
+            else
+                tail.Next = elementMyClass;
+            
+            tail = elementMyClass;
+
+            count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
