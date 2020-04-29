fec60b1b8c942cd993c69446cfc7d78ab1d5c3cf
GradilSvyat <43150486+GradilSvyat@users.noreply.github.com>
2020-04-29 14:01:02 +0300

    output results into console

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyListGeneric;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyClassGeneric<string> firstElement = new MyClassGeneric<string>("This is the first element");
+            MyClassGeneric<string> secondElement = new MyClassGeneric<string>("This is the second element");
+            MyClassGeneric<string> thirdElement = new MyClassGeneric<string>("This is the third element");
+
+            MyList <MyClassGeneric<string> > myCollection = new MyList<MyClassGeneric<string>>();
+            myCollection.Add(firstElement);
+            myCollection.Add(secondElement);
+            myCollection.Add(thirdElement);
+
+            Console.WriteLine("\nВывод коллекции после добавления элементов:\n");
+            foreach(var item in myCollection)
+            {
+                Console.WriteLine("\t" + item.Data);
+            }
+
+            myCollection.Remove(secondElement);
+            Console.WriteLine("\nВывод коллекции после удаления второго элемента:\n");
+            foreach (var item in myCollection)
+            {
+                Console.WriteLine("\t" + item.Data);
+            }
+
         }
     }
 }
