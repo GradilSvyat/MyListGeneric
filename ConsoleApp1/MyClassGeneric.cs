3ab4b57fa11b7139fd1a01b2244ca2c7a4e8b2ec
GradilSvyat <43150486+GradilSvyat@users.noreply.github.com>
2020-04-29 12:13:18 +0300

    Create generic class

---
+++ ConsoleApp1/MyClassGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyListGeneric
+{
+    class MyClassGeneric <T>
+    {
+        public T Data { get; set; }
+        public MyClassGeneric <T> Next { get; set; }
+        public MyClassGeneric (T data)
+        {
+            Data = data;
+        }
+    }
+}
